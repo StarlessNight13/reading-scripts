0c19a6fae224487b05f7d8f561d7a22835d021db
Starless Night <DarklessNight13@proton.me>
2025-11-21 07:19:46 +0000

    should be good enough to make a realase wihtout the kolnovel beta

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import kolnovel from "./beta_kolnovel";
+import kolnovel from "./kolnovel";
 import cenel from "./cenel";
 import "./index.css";
+import tailwindcss from "./tailwind.css?raw";
 
 (async () => {
   const website = location.hostname;
   if (website === "kolnovel.com") {
-    kolnovel();
+    kolnovel(tailwindcss);
   } else if (website === "cenele.com") {
     cenel();
   } else {
